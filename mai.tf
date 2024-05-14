4863ddbc7ab2591d181a6538fc746f9dfd219537
Ravi kumar <119988156+gravikumar12@users.noreply.github.com>
2024-05-14 13:05:26 +0530

    Create mai.tf

---
+++ mai.tf
@@ -0,0 +1,46 @@
+resource "google_container_cluster" "demo_cluster" {
+  project  = "" # Replace with your Project ID, https://cloud.google.com/resource-manager/docs/creating-managing-projects#identifying_projects
+  name     = "demo-cluster"
+  location = "us-west1-a"
+
+  min_master_version = "1.27"
+
+  # Enable Alias IPs to allow Windows Server networking.
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "/14"
+    services_ipv4_cidr_block = "/20"
+  }
+
+  # Removes the implicit default node pool, recommended when using
+  # google_container_node_pool.
+  remove_default_node_pool = true
+  initial_node_count = 1
+}
+
+# Small Linux node pool to run some Linux-only Kubernetes Pods.
+resource "google_container_node_pool" "linux_pool" {
+  name               = "linux-pool"
+  project            = google_container_cluster.demo_cluster.project
+  cluster            = google_container_cluster.demo_cluster.name
+  location           = google_container_cluster.demo_cluster.location
+
+  node_config {
+    image_type   = "COS_CONTAINERD"
+  }
+}
+
+# Node pool of Windows Server machines.
+resource "google_container_node_pool" "windows_pool" {
+  name               = "windows-pool"
+  project            = google_container_cluster.demo_cluster.project
+  cluster            = google_container_cluster.demo_cluster.name
+  location           = google_container_cluster.demo_cluster.location
+
+  node_config {
+    machine_type = "e2-standard-4"
+    image_type   = "WINDOWS_LTSC" # Or WINDOWS_SAC for new features.
+  }
+
+  # The Linux node pool must be created before the Windows Server node pool.
+  depends_on = [google_container_node_pool.linux_pool]
+}
